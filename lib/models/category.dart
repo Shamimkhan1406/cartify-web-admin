b916e854f0cfe641d367cceb0d6d5728ec4476d1
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-14 22:07:44 +0530

    serialize and deserialize model

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,9 +1,34 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
 
-class Category{
+
+class Category {
   final String id;
   final String name;
   final String image;
   final String banner;
 
   Category(this.id, this.name, this.image, this.banner);
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'image': image,
+      'banner': banner,
+    };
+  }
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(
+      map['id'] as String,
+      map['name'] as String,
+      map['image'] as String,
+      map['banner'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Category.fromJson(String source) => Category.fromMap(json.decode(source) as Map<String, dynamic>);
 }
