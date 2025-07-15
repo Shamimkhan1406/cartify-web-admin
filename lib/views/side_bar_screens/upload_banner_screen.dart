62434d83651b449b53c32d99743a269cc7b91066
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-15 21:30:07 +0530

    banner screen ui

--- lib/views/side_bar_screens/upload_banner_screen.dart
+++ lib/views/side_bar_screens/upload_banner_screen.dart
@@ -6,8 +6,76 @@ class UploadBannerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Upload Banner Screen'),
+    return SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Banner',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Divider(color: Colors.grey),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(
+                        child: Text('Image'),
+                      ),
+                    ),
+                    
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          // Implement save functionality
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff051247),
+                        ),
+                        child: Text(
+                          'Save',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      //;
+                    },
+                    child: Text('Upload Image'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Divider(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
     );
   }
 }
