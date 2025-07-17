b8d55392db85abced2d17408c85e979c93f2e6af
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-07-18 00:51:32 +0530

    fetch main category and display dropdown

--- lib/views/side_bar_screens/sub_category_screens.dart
+++ lib/views/side_bar_screens/sub_category_screens.dart
@@ -1,3 +1,6 @@
+import 'package:cartify_web/controller/category_controller.dart';
+import 'package:cartify_web/models/category.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class SubCategoryScreens extends StatefulWidget {
@@ -9,8 +12,140 @@ class SubCategoryScreens extends StatefulWidget {
 }
 
 class _SubCategoryScreensState extends State<SubCategoryScreens> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  late Future<List<Category>> futureCategories;
+  late String name;
+  Category? selectedCategory;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    futureCategories = CategoryController().loadCategories();
+  }
+
+  dynamic _image;
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+    );
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("subCategoryScreen"));
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Sub Categories',
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Divider(color: Colors.grey),
+            FutureBuilder(
+              future: futureCategories,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(
+                    child: Text('No sub categories available'),
+                  );
+                } else {
+                  return DropdownButton<Category>(
+                    hint: Text('Select Category'),
+                    items:
+                        snapshot.data!.map((Category category) {
+                          return DropdownMenuItem(
+                            value: category,
+                            child: Text(category.name),
+                          );
+                        }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedCategory = value;
+                      });
+                      print(selectedCategory!.name);
+                    },
+                  );
+                }
+              },
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child:
+                        _image != null ? Image.memory(_image) : Text('SubCategoryImage'),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: 200,
+                    child: TextFormField(
+                      onChanged: (value) {
+                        name = value;
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter Category Name';
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: const InputDecoration(
+                        labelText: 'Enter Sub Category Name',
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      //
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xff051247),
+                  ),
+                  child: Text('Save', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  pickImage();
+                },
+                child: Text('Upload Image'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
